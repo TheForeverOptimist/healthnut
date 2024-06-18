10aed5c6a0a840ab3204a8032b3589c5ae0b2834
TheForeverOptimist <sean@elevate.app>
2024-06-17 21:34:01 -0700

    updating redirects

--- src/app/api/auth/callback/route.ts
+++ src/app/api/auth/callback/route.ts
@@ -1,14 +1,17 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
 
-export default async function callbackHandler(req: NextApiRequest, res: NextApiResponse){
-    const session = await getSession({req});
+export default async function callbackHandler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req });
 
-    if(session){
-        //lets redirect to the dashboard if authentication is successful
-        res.redirect('/dashboard');
-    }else{
-        //redirect back to homepage
-        res.redirect('/')
-    }
+  if (session) {
+    //lets redirect to the dashboard if authentication is successful
+    res.redirect("/dashboard");
+  } else {
+    //redirect back to homepage
+    res.redirect("/");
+  }
 }
