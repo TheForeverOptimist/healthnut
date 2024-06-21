103048f4d36a87852d9288a66154d836a763f418
TheForeverOptimist <sean@elevate.app>
2024-06-20 23:34:30 -0700

    added cookie check client side to keep logged in user from having to continuously log in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
 "use client";
 
 import React from "react";
-import { useState } from "react";
 import Header from "./components/Header/header";
 import Hero from "./components/Hero/hero";
-import Dashboard from "./Dashboard/page";
-import Invite from "./Invite/page";
-import LoginForm from "./components/LoginForm/loginform";
+import { parseClientCookies } from "@/libs/cookies";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
 
 export default function Home(): JSX.Element {
+  const router = useRouter()
+
+  useEffect(() => {
+    const cookies = parseClientCookies();
+    const medplumProfile = cookies.medplumProfile;
+    const medplumLogin = cookies.medplumLogin;
+
+    if(medplumProfile || medplumLogin){
+      router.push('/Dashboard')
+    }
+  }, [router])
 
   return (
     <main className="container">
